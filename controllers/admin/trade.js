175b0460ad5b0802f05b4f1ea573b7083d5d3b4e
wx2243612 <37129779+wx2243612@users.noreply.github.com>
2018-07-02 15:05:36 +0800

    Update trade.js

--- controllers/admin/trade.js
+++ controllers/admin/trade.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Account  = mongoose.model('Account');
@@ -11,35 +10,35 @@ const co = require('co');
 const async = require('co').wrap;
 const only = require('only');
 const accountLib = require('../../lib/account');
+const transfer = require('../../lib/transfer');
 const order = require('../../lib/order');
 const apiConfigUtil = require('../../lib/apiClient/apiConfigUtil');
 const transferController = require('../../lib/transferStrategys/transferController');
 
 module.exports = function (router) {
-    //router.get('/', account.index_api);
-
     router.get('/', async(function* (req, res) {
         try{
-            list(req,res,function(data){
-                res.render('admin/trade-1',data);
+            list(req,res,function(result){
+                res.render('admin/wtlb',result);
             });
+            
         } catch(err){
             console.error(err);
             res.json({ isSuccess: false, code: 500, message: "500:服务器端发生错误"});
         }
     }));
 
-    router.get('/list', async(function* (req, res) {
+    router.post('/list', async(function* (req,res){
         try{
-            list(req,res,function(data){
-                res.json(data);     
+            list(req,res,function(result){
+                res.json(result);
             });
+            
         } catch(err){
             console.error(err);
             res.json({ isSuccess: false, code: 500, message: "500:服务器端发生错误"});
-        }
+        } 
     }));
-
     
     router.post('/syncRecentOrders', async function(req, res) {
         try{
@@ -78,43 +77,30 @@ module.exports = function (router) {
 }
 
 function list(req,res,callback){
-    let sPageIndex = req.query.pageIndex;
-    let sPageSize = req.query.pageSize;
-    let pageIndex = Number(sPageIndex) || 0;
-    let pageSize = Number(sPageSize) || 10;
+    let pageNumber = Number(req.body.page || '1') || 1;
+    let pageSize = Number(req.body.rp || '10') || 10;
 
     let userName = req.user.userName;
-    let status = req.query.status;
-    let site = req.query.site;
 
-    if(!userName){
-        callback({ isSuccess: false,code: "100007",message: "用户尚未登录" });
-        return;
-    }
-
-    let filters = { userName : userName }
-    if(status){
-        filters.status = status;
-    }
-
-    if(site){
-        filters.site = site;
-    }
+    let query = { "$where": function(){
+        return true;
+    }};
 
     var options = {
         //select:   'title date author',
-        sort: { modified : -1 },
+        sort: { created : -1 },
         //populate: 'strategyId',
         lean: true,
-        page: pageIndex + 1, 
+        page: pageNumber, 
         limit: pageSize
     };
 
-    Order.paginate(filters, options).then(function(getRes) {
+    Order.paginate(query, options).then(async function(getRes) {
+        let orders = getRes.docs || [];
         let t = {
             pageSize: getRes.limit,
             total: getRes.total,
-            orders: getRes.docs,
+            orders: JSON.stringify(orders || []),
             isSuccess: true
         };  
 
